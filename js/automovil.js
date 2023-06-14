1e5e0cdde0216b4eb6ec69d8006ea267ef18b561
cristianranged <crangeld@ufpso.edu.co>
2023-06-13 22:50:40 -0500

    validaciones de inicio de sesión y actualización de usuarios

--- js/automovil.js
+++ js/automovil.js
@@ -1,5 +1,5 @@
 //funciones propias de la app
-const urlApi = "http://localhost:8081";//colocar la url con el puerto
+const urlApi = "http://localhost:8088";//colocar la url con el puerto
 
 async function login(){
     var myForm = document.getElementById("myForm");
@@ -19,16 +19,31 @@ async function login(){
     const request = await fetch(urlApi+"/auth/login",settings);
     //console.log(await request.text());
     if(request.ok){
+        Swal.fire({
+            position: 'center',
+            icon: 'success',
+            title: 'logeado exitosamente',
+            showConfirmButton: false,
+            timer: 3500
+          })
         const respuesta = await request.json();
         localStorage.token = respuesta.data.token;
 
         //localStorage.token = respuesta;
         localStorage.email = jsonData.email;      
         location.href= "dashboard.html";
+    }else{
+        Swal.fire({
+            position: 'center',
+            icon: 'error',
+            text: 'Datos incorrectos',
+            showConfirmButton: false,
+            timer: 3000
+          });
     }
 }
 
-function listar(){
+function listarAutomoviles(){
     validaToken();
     var settings={
         method: 'GET',
@@ -38,19 +53,24 @@ function listar(){
             'Authorization': localStorage.token
         },
     }
-    fetch(urlApi+"/user",settings)
+    fetch(urlApi+"/carss/1",settings)
     .then(response => response.json())
-    .then(function(users){
+    .then(function(cars){
         
-            var usuarios = '';
-            for(const usuario of users.data){
-                console.log(usuario.email)
-                usuarios += `
+            var automoviles = '';
+            for(const automovil of cars.data){
+                console.log(automovil.id)
+                automoviles += `
                 <tr>
-                    <th scope="row">${usuario.id}</th>
-                    <td>${usuario.firstName}</td>
-                    <td>${usuario.lastName}</td>
-                    <td>${usuario.email}</td>
+                    <th scope="row">${automovil.id}</th>
+                    <td>${automovil.car}</td>
+                    <td>${automovil.car_model}</td>
+                    <td>${automovil.car_color}</td>
+                    <td>${automovil.car_model_year}</td>
+                    <td>${automovil.car_model_year}</td>
+                    <td>${automovil.car_vin}</td>
+                    <td>${automovil.price}</td>
+                    <td>${automovil.availability}</td>
                     <td>
                     <button type="button" class="btn btn-outline-danger" 
                     onclick="eliminaUsuario('${usuario.id}')">
@@ -66,7 +86,7 @@ function listar(){
                 </tr>`;
                 
             }
-            document.getElementById("listar").innerHTML = usuarios;
+            document.getElementById("listarAutomoviles").innerHTML = automovil;
     })
 }
 
@@ -204,31 +224,49 @@ function alertas(mensaje,tipo){
     document.getElementById("datos").innerHTML = alerta;
 }
 
-function registerForm(){
+function registerForm1(){
     cadena = `
             <div class="p-3 mb-2 bg-light text-dark">
                 <h1 class="display-5"><i class="fa-solid fa-user-pen"></i> User Register</h1>
             </div>
               
-            <form action="" method="post" id="registerForm">
+            <form action="" method="post" id="myform2">
+
                 <input type="hidden" name="id" id="id">
-                <label for="firstName" class="form-label">First Name</label>
-                <input type="text" class="form-control" name="firstName" id="firstName" required> <br>
-                <label for="lastName"  class="form-label">Last Name</label>
-                <input type="text" class="form-control" name="lastName" id="lastName" required> <br>
-                <label for="email" class="form-label">Email</label>
-                <input type="email" class="form-control" name="email" id="email" required> <br>
-                <label for="password" class="form-label">Password</label>
-                <input type="password" class="form-control" id="password" name="password" required> <br>
-                <button type="button" class="btn btn-outline-info" onclick="registrarUsuario()">Registrar</button>
+
+                <label for="car" class="form-label">Car</label>
+                <input type="text" class="form-control" name="Car" id="car" required> <br>
+
+                <label for="car_model"  class="form-label">Car Model</label>
+                <input type="text" class="form-control" name="Car Model" id="car_model" required> <br>
+
+                <label for="car_color" class="form-label">Car color</label>
+                <input type="car_color" class="form-control" name="car color" id="car_color" required> <br>
+
+                <label for="car_model_year" class="form-label">car model year</label>
+                <input type="number" class="form-control" id="car_model_year" name="car model year" required> <br>
+
+                <label for="car_vin" class="form-label">car vin year</label>
+                <input type="text" class="form-control" id="car_vin" name="car vin " required> <br>
+
+                <label for="car_model_year" class="form-label">car model year</label>
+                <input type="number" class="form-control" id="car_model_year" name="car model year" required> <br>
+
+                <label for="price" class="form-label">price</label>
+                <input type="number" class="form-control" id="price" name="price" required> <br>
+
+                <label for="availability" class="form-label">availability</label>
+                <input type="number" class="form-control" id="price" name="price" required> <br>
+
+                <button type="button" class="btn btn-outline-info" onclick="registrarAutomovil()">Registrar</button>
             </form>`;
             document.getElementById("contentModal").innerHTML = cadena;
             var myModal = new bootstrap.Modal(document.getElementById('modalUsuario'))
             myModal.toggle();
 }
 
-async function registrarUsuario(){
-    var myForm = document.getElementById("registerForm");
+async function registrarAutomovil(){
+    var myForm = document.getElementById("myform1");
     var formData = new FormData(myForm);
     var jsonData = {};
     for(var [k, v] of formData){//convertimos los datos a json
